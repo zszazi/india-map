932912aa1a7a543832723683f2d4c24527bde971
zszazi <41579863+zszazi@users.noreply.github.com>
2019-05-01 22:27:23 +0530

    Update map.c

--- map.c
+++ map.c
@@ -3,6 +3,7 @@
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
+This Code was copied from a public forum where it was pasted
 
 *******************************************************************************/
 #include <stdio.h> 
